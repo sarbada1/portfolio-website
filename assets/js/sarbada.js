328dc2388da53cbc5b5749f0fbdc8da8e3414508
sarbada1 <sanjelsarbada12@gmail.com>
2024-06-04 16:17:50 +0545

    image update

--- assets/js/sarbada.js
+++ assets/js/sarbada.js
@@ -61,3 +61,26 @@ $(document).ready(function () {
           alert("Failed to send message. Please try again later.");
         });
     });
+    document.addEventListener("DOMContentLoaded", function() {
+      var lazyImages = [].slice.call(document.querySelectorAll("img.lazy-load"));
+    
+      if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+          entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+              let lazyImage = entry.target;
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.classList.remove("lazy-load");
+              lazyImageObserver.unobserve(lazyImage);
+            }
+          });
+        });
+    
+        lazyImages.forEach(function(lazyImage) {
+          lazyImageObserver.observe(lazyImage);
+        });
+      } else {
+        // Possibly fall back to event handlers here
+      }
+    });
+    
